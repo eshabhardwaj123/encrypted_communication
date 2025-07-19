9c56c56ff02bb7ad25471e37af74f57d92c1a0d0
Esha Bhardwaj <148773452+eshabhardwaj123@users.noreply.github.com>
2025-07-20 00:36:11 +0530

    bidirectional communication
    bydirectional communication with aes encryption and decryption done

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 import threading
-from crypto_utils import generate_key_pair, derive_shared_key, encrypt_message_aes, decrypt_message_aes, serialize_pub_key, deserialize_pub_key
+from crypto_utils import generate_keys, derive_shared_key, encrypt_message_aes, decrypt_message_aes, serialize_public_key, deserialize_public_key
 
 HOST = '127.0.0.1'
 PORT = 65432
 
-private_key, public_key = generate_key_pair()
+private_key, public_key = generate_keys()
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
@@ -13,8 +13,8 @@ print("[CLIENT] Connected to server.")
 
 # Exchange public keys
 server_pub_key_bytes = client.recv(1024)
-client.sendall(serialize_pub_key(public_key))
-server_pub_key = deserialize_pub_key(server_pub_key_bytes)
+client.sendall(serialize_public_key(public_key))
+server_pub_key = deserialize_public_key(server_pub_key_bytes)
 
 # Derive AES key
 shared_key = derive_shared_key(private_key, server_pub_key)
@@ -22,10 +22,8 @@ shared_key = derive_shared_key(private_key, server_pub_key)
 def receive_messages():
     while True:
         try:
-            nonce = client.recv(12)
-            tag = client.recv(16)
-            ciphertext = client.recv(1024)
-            decrypted = decrypt_message_aes(shared_key, nonce, ciphertext, tag)
+            encrypted_data = client.recv(1024 + 12)
+            decrypted = decrypt_message_aes(shared_key, encrypted_data)
             print(f"[SERVER]: {decrypted}")
         except Exception as e:
             print("[ERROR RECEIVING]", e)
@@ -34,10 +32,8 @@ def receive_messages():
 def send_messages():
     while True:
         msg = input("[YOU]: ")
-        nonce, ciphertext, tag = encrypt_message_aes(shared_key, msg)
-        client.sendall(nonce)
-        client.sendall(tag)
-        client.sendall(ciphertext)
+        encrypted_data = encrypt_message_aes(shared_key, msg)
+        client.sendall(encrypted_data)
 
 recv_thread = threading.Thread(target=receive_messages)
 recv_thread.start()
